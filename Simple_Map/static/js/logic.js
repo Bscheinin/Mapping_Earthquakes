2f513a85690e7597c1edc690c1cf72e0093bda3f
Scheinin <scheir3@medtronic.com>
2022-03-18 19:09:31 -0600

    Homework 5

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,21 +2,49 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-   let line = [
-        [37.6213, -122.3790],
-        [30.2666, -97.7333],
-        [39.8493, -104.6738],
-        [44.1643, -80.0369],
-        [40.6413, -73.7781]
-      ];
-      // Create a polyline using the line coordinates and make the line red.
-    L.polyline(line, {
-        color: 'blue',
-        weight: 4,
-        dashArray: '20, 20'
-      }).addTo(map);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup();
+    }
+}).addTo(map);
+
+
+//    let line = [
+//         [37.6213, -122.3790],
+//         [30.2666, -97.7333],
+//         [39.8493, -104.6738],
+//         [44.1643, -80.0369],
+//         [40.6413, -73.7781]
+//       ];
+//       // Create a polyline using the line coordinates and make the line red.
+//     L.polyline(line, {
+//         color: 'blue',
+//         weight: 4,
+//         dashArray: '20, 20'
+//       }).addTo(map);
 
 // Coordinates for each point to be used in the line.
 //   let line = [
@@ -64,10 +92,10 @@ let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
  
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/navigation-night-v1',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
